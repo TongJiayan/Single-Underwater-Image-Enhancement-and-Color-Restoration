8ccdbf81eee5de3be58bf450b1bd5a3421e57f75
Tongjiayan <jiayan_tong@163.com>
2021-08-08 18:02:49 +0800

    update DCP

--- Underwater Image Color Restoration/DCP/main.py
+++ Underwater Image Color Restoration/DCP/main.py
@@ -1,4 +1,5 @@
 from GuidedFilter import GuidedFilter
+from evaluation import getScore
 
 __author__ = 'wy'
 import datetime
@@ -12,6 +13,8 @@ import numpy as np
 import cv2
 import natsort
 import xlwt
+import argparse
+import time
 
 
 # 用于排序时存储原来像素点位置的数据结构
@@ -34,16 +37,8 @@ def getMinChannel(img):
     else:
         print("bad image shape, input must be color image")
         return None
-    imgGray = np.zeros((img.shape[0], img.shape[1]), dtype=np.uint8)
-    localMin = 255
+    imgGray = img.min(axis=2)
 
-    for i in range(0, img.shape[0]):
-        for j in range(0, img.shape[1]):
-            localMin = 255
-            for k in range(0, 3):
-                if img.item((i, j, k)) < localMin:
-                    localMin = img.item((i, j, k))
-            imgGray[i, j] = localMin
     return imgGray
 
 
@@ -60,7 +55,7 @@ def getDarkChannel(img, blockSize):
     if blockSize % 2 == 0 or blockSize < 3:
         print('blockSize is not odd or too small')
         return None
-    # print('blockSize', blockSize)
+    
     # 计算addSize
     addSize = int((blockSize - 1) / 2)
     newHeight = img.shape[0] + blockSize - 1
@@ -69,25 +64,15 @@ def getDarkChannel(img, blockSize):
     # 中间结果
     imgMiddle = np.zeros((newHeight, newWidth))
     imgMiddle[:, :] = 255
-    # print('imgMiddle',imgMiddle)
-    # print('type(newHeight)',type(newHeight))
-    # print('type(addSize)',type(addSize))
     imgMiddle[addSize:newHeight - addSize, addSize:newWidth - addSize] = img
-    # print('imgMiddle', imgMiddle)
     imgDark = np.zeros((img.shape[0], img.shape[1]), np.uint8)
-    localMin = 255
-
-    for i in range(addSize, newHeight - addSize):
-        for j in range(addSize, newWidth - addSize):
-            localMin = 255
-            for k in range(i - addSize, i + addSize + 1):
-                for l in range(j - addSize, j + addSize + 1):
-                    if imgMiddle.item((k, l)) < localMin:
-                        localMin = imgMiddle.item((k, l))
-            imgDark[i - addSize, j - addSize] = localMin
-
+    
+    # 计算每个block中最小值
+    for i in range(img.shape[0]):
+        for j in range(img.shape[1]):
+            imgDark[i,j] = imgMiddle[i:i+blockSize, j:j+blockSize].min()
+   
     return imgDark
-
 
 # 获取全局大气光强度
 def getAtomsphericLight(darkChannel, img, meanMode=False, percent=0.001):
@@ -142,15 +127,17 @@ def getRecoverScene(img, omega=0.95, t0=0.1, blockSize=15, meanMode=False, perce
     gimfiltR = 50  # 引导滤波时半径的大小
     eps = 10 ** -3  # 引导滤波时epsilon的值
     
-    
+    print("Get MinChannel...")
     imgGray = getMinChannel(img)
-    # print('imgGray', imgGray)
+    print("Get DarkChannel...")
     imgDark = getDarkChannel(imgGray, blockSize=blockSize)
+    print("Get AtomsphericLight...")
     atomsphericLight = getAtomsphericLight(imgDark, img, meanMode=meanMode, percent=percent)
 
     imgDark = np.float64(imgDark)
     transmission = 1 - omega * imgDark / atomsphericLight
 
+    print("Get Transmision...")
     guided_filter = GuidedFilter(img, gimfiltR, eps)
     transmission = guided_filter.filter(transmission)
     
@@ -159,6 +146,7 @@ def getRecoverScene(img, omega=0.95, t0=0.1, blockSize=15, meanMode=False, perce
 
     transmission = np.clip(transmission, t0, 0.9)
 
+    print("Get sceneRadiance...")
     sceneRadiance = np.zeros(img.shape)
     for i in range(0, 3):
         img = np.float64(img)
@@ -170,24 +158,50 @@ def getRecoverScene(img, omega=0.95, t0=0.1, blockSize=15, meanMode=False, perce
     return transmission,sceneRadiance
 
 
+def run(args):
+    root_path = "/home/tongjiayan/UnderwaterImageRestoration/densedepth_based_restoration/datasets"
+    dataset_path = os.path.join(root_path, args.dataset)
+    score = open(args.score, mode = 'a',encoding='utf-8')
+    np.seterr(over='raise') # 触发FloatingPointError
+    time_start = time.time()
+    for image_name in os.listdir(dataset_path):
+        image_path = os.path.join(dataset_path, image_name)
+        if os.path.isfile(image_path):
+            print('********    file   ********',image_name)
+            img = cv2.imread(image_path) # BGR格式
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            print("Get score...")
+            UICM, UISM, UIConM, UIQM, UCIQE = getScore(np.array(img))
+            print(image_name, file=score)
+            print("{0}\t{1}\t{2}\t{3}\t{4}".format(UICM, UISM, UIConM, UIQM, UCIQE), file=score)
+            print("Finished to get score!")
+            try:
+                transmission, sceneRadiance = getRecoverScene(img)
+            except ValueError:
+                print("Fail to recover " + image_name + "!")
+            else:
+                save_path = os.path.join("OutputImages", args.dataset)
+                output_image = image_name.split('.')[0] + '_DCP.jpg'
+                output_depth = image_name.split('.')[0] + '_DCP_TM.jpg'
+                print(os.path.join(save_path, output_depth))
+                cv2.imwrite(os.path.join(save_path, output_depth), np.uint8(transmission * 255))
 
-np.seterr(over='ignore')
-if __name__ == '__main__':
-    pass
+                UICM, UISM, UIConM, UIQM, UCIQE = getScore(np.array(sceneRadiance))
+                print("{0}\t{1}\t{2}\t{3}\t{4}".format(UICM, UISM, UIConM, UIQM, UCIQE), file=score)
+                sceneRadiance = cv2.cvtColor(sceneRadiance, cv2.COLOR_RGB2BGR)
+                cv2.imwrite(os.path.join(save_path, output_image), sceneRadiance)
+                print("Done!")
+    
+    time_end = time.time()
+    print("TIME COST = {0}".format(time_end-time_start), file=score)
+    score.close()
 
-# folder = "C:/Users/Administrator/Desktop/UnderwaterImageEnhancement/Physical/DCP"
-folder = "C:/Users/Administrator/Desktop/Databases/Dataset"
-path = folder + "/InputImages"
-files = os.listdir(path)
-files =  natsort.natsorted(files)
+if __name__=='__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--score', default='score.log', help='score log')
+    parser.add_argument('--dataset', default='SQUID', help='Input image directory')
+    args = parser.parse_args()
+    run(args)
 
-for i in range(len(files)):
-    file = files[i]
-    filepath = path + "/" + file
-    prefix = file.split('.')[0]
-    if os.path.isfile(filepath):
-        print('********    file   ********',file)
-        img = cv2.imread(folder +'/InputImages/' + file)
-        transmission, sceneRadiance = getRecoverScene(img)
-        cv2.imwrite('OutputImages/' + prefix + '_DCP_TM.jpg', np.uint8(transmission * 255))
-        cv2.imwrite('OutputImages/' + prefix + '_DCP.jpg', sceneRadiance)
+    
+
