306c243cbdb71a97fa464df62afb4ed18bd9929b
Tongjiayan <jiayan_tong@163.com>
2021-09-26 17:45:53 +0800

    add GBdehazing and ULAP

--- Underwater Image Color Restoration/GBdehazingRCorrection/main.py
+++ Underwater Image Color Restoration/GBdehazingRCorrection/main.py
@@ -3,6 +3,10 @@ import datetime
 import numpy as np
 import cv2
 import natsort
+import argparse
+import time
+
+
 from DetermineDepth import determineDepth
 from TransmissionEstimation import getTransmission
 from getAdaptiveExposureMap import AdaptiveExposureMap
@@ -13,65 +17,74 @@ from refinedTransmission import refinedtransmission
 from sceneRadianceGb import sceneRadianceGB
 from sceneRadianceR import sceneradiance
 
+from evaluation import getScore
+
 # # # # # # # # # # # # # # # # # # # # # # Normalized implement is necessary part as the fore-processing   # # # # # # # # # # # # # # # #
+# 恢复原图像
+# img : BGR格式
+def getRecoverScene(img, blockSize=9, n=5):
+    img = (img - img.min()) / (img.max() - img.min()) * 255
+    blockSize = 9
+    largestDiff = determineDepth(img, blockSize)
+    AtomsphericLight, AtomsphericLightGB, AtomsphericLightRGB = getAtomsphericLight(largestDiff, img)
+    print('AtomsphericLightRGB',AtomsphericLightRGB)
+    
+    # transmission = getTransmission(img, AtomsphericLightRGB, blockSize=blockSize)
+    transmission = getTransmission(img, AtomsphericLightRGB, blockSize)
+    # print('transmission.shape',transmission.shape)
+    # TransmissionComposition(folder, transmission, number, param='coarse')
+    transmission = refinedtransmission(transmission, img)
+    sceneRadiance_GB = sceneRadianceGB(img, transmission, AtomsphericLightRGB)
 
-np.seterr(over='ignore')
-if __name__ == '__main__':
-    pass
-
-starttime = datetime.datetime.now()
-
-# folder = "C:/Users/Administrator/Desktop/UnderwaterImageEnhancement/Physical/GBdehazingRCorrection"
-folder = "C:/Users/Administrator/Desktop/Databases/Dataset"
-path = folder + "/InputImages"
-files = os.listdir(path)
-files =  natsort.natsorted(files)
-
-for i in range(len(files)):
-    file = files[i]
-    filepath = path + "/" + file
-    prefix = file.split('.')[0]
-    if os.path.isfile(filepath):
-        print('********    file   ********',file)
-        img = cv2.imread(folder +'/InputImages/' + file)
-        img = (img - img.min()) / (img.max() - img.min()) * 255
-        blockSize = 9
-        largestDiff = determineDepth(img, blockSize)
-        AtomsphericLight, AtomsphericLightGB, AtomsphericLightRGB = getAtomsphericLight(largestDiff, img)
-        print('AtomsphericLightRGB',AtomsphericLightRGB)
-        
-        # transmission = getTransmission(img, AtomsphericLightRGB, blockSize=blockSize)
-        transmission = getTransmission(img, AtomsphericLightRGB, blockSize)
-        # print('transmission.shape',transmission.shape)
-        # TransmissionComposition(folder, transmission, number, param='coarse')
-        transmission = refinedtransmission(transmission, img)
-
-        cv2.imwrite('OutputImages/' + prefix + '_GBDehazedRcoorectionUDCP_TM.jpg', np.uint8(transmission[:, :, 0] * 255))
+    # cv2.imwrite('OutputImages/' + prefix + 'GBDehazed.jpg', sceneRadiance_GB)
 
 
-        # TransmissionComposition(folder, transmission, number, param='refined_15_175_175')
-        sceneRadiance_GB = sceneRadianceGB(img, transmission, AtomsphericLightRGB)
+    # # print('sceneRadiance_GB',sceneRadiance_GB)
+    sceneRadiance = sceneradiance(img, sceneRadiance_GB)
+    S_x = AdaptiveExposureMap(img, sceneRadiance, Lambda=0.3, blockSize=blockSize)
+    # print('S_x',S_x)
+    sceneRadiance = AdaptiveSceneRadiance(sceneRadiance, S_x)
 
-        # cv2.imwrite('OutputImages/' + prefix + 'GBDehazed.jpg', sceneRadiance_GB)
+    return sceneRadiance
 
+def run(args):
+    root_path = "/home/tongjiayan/UnderwaterImageRestoration/densedepth_based_restoration/datasets"
+    dataset_path = os.path.join(root_path, args.dataset)
+    score = open(args.score, mode = 'a',encoding='utf-8')
+    np.seterr(over='raise') # 触发FloatingPointError
+    time_start = time.time()
+    for image_name in os.listdir(dataset_path):
+        image_path = os.path.join(dataset_path, image_name)
+        if os.path.isfile(image_path):
+            print('********    file   ********',image_name)
+            img = cv2.imread(image_path) # BGR格式
+            img_RGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) # 转换成RGB格式
+            print("Get score...")
+            UICM, UISM, UIConM, UIQM, UCIQE = getScore(np.array(img_RGB))
+            print(image_name, file=score)
+            print("{0}\t{1}\t{2}\t{3}\t{4}".format(UICM, UISM, UIConM, UIQM, UCIQE), file=score)
+            print("Finished to get score!")
+            try:
+                sceneRadiance = getRecoverScene(img)
+            except FloatingPointError:
+                print("Fail to recover " + image_name + "!")
+            else:
+                sceneRadiance_RGB = cv2.cvtColor(sceneRadiance, cv2.COLOR_BGR2RGB)
+                UICM, UISM, UIConM, UIQM, UCIQE = getScore(np.array(sceneRadiance_RGB))
+                print("{0}\t{1}\t{2}\t{3}\t{4}".format(UICM, UISM, UIConM, UIQM, UCIQE), file=score)
+                save_path = os.path.join("OutputImages", args.dataset)
+                output_image = image_name.split('.')[0] + '_GBdehazing.jpg'
+                cv2.imwrite(os.path.join(save_path, output_image), sceneRadiance)
+                print("Done!")
+    
+    time_end = time.time()
+    print("TIME COST = {0}".format(time_end-time_start), file=score)
+    score.close()
 
-        # # print('sceneRadiance_GB',sceneRadiance_GB)
-        sceneRadiance = sceneradiance(img, sceneRadiance_GB)
-        # sceneRadiance= sceneRadiance_GB
-        # cv2.imwrite('OutputImages/'+ prefix + 'GBDehazedRcoorectionUDCP.jpg', sceneRadiance)
-        # # print('np.min(sceneRadiance)',np.min(sceneRadiance))
-        # # print('sceneRadiance',sceneRadiance)
-        
-        S_x = AdaptiveExposureMap(img, sceneRadiance, Lambda=0.3, blockSize=blockSize)
-        # print('S_x',S_x)
-        sceneRadiance = AdaptiveSceneRadiance(sceneRadiance, S_x)
-        
-        # print('sceneRadiance',sceneRadiance)
-        cv2.imwrite('OutputImages/' + prefix + 'GBDehazedRcoorectionUDCPAdaptiveMap.jpg', sceneRadiance)
-
-
-Endtime = datetime.datetime.now()
-Time = Endtime - starttime
-print('Time', Time)
-
+if __name__=='__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--score', default='score.log', help='score log')
+    parser.add_argument('--dataset', default='SQUID', help='Input image directory')
+    args = parser.parse_args()
+    run(args)
 
--- Underwater Image Color Restoration/ULAP/main.py
+++ Underwater Image Color Restoration/ULAP/main.py
@@ -4,6 +4,8 @@ import datetime
 import numpy as np
 import cv2
 import natsort
+import argparse
+import time
 
 from GuidedFilter import GuidedFilter
 from backgroundLight import BLEstimation
@@ -14,59 +16,68 @@ from global_Stretching import global_stretching
 from refinedTransmissionMap import refinedtransmissionMap
 
 from sceneRadiance import sceneRadianceRGB
+from evaluation import getScore
 
-np.seterr(over='ignore')
-if __name__ == '__main__':
-    pass
+def getRecoverScene(img):
+    gimfiltR = 50  # 引导滤波时半径的大小
+    eps = 10 ** -3  # 引导滤波时epsilon的值
 
-starttime = datetime.datetime.now()
+    DepthMap = depthMap(img)
+    DepthMap = global_stretching(DepthMap)
+    guided_filter = GuidedFilter(img, gimfiltR, eps)
+    refineDR = guided_filter.filter(DepthMap)
+    refineDR = np.clip(refineDR, 0,1)
 
-# folder = "C:/Users/Administrator/Desktop/UnderwaterImageEnhancement/Physical/ULAP"
-folder = "C:/Users/Administrator/Desktop/Databases/Dataset"
-path = folder + "/InputImages"
-files = os.listdir(path)
-files =  natsort.natsorted(files)
+    AtomsphericLight = BLEstimation(img, DepthMap) * 255
 
-for i in range(len(files)):
-    file = files[i]
-    filepath = path + "/" + file
-    prefix = file.split('.')[0]
-    if os.path.isfile(filepath):
-        print('********    file   ********',file)
-        img = cv2.imread(folder +'/InputImages/' + file)
+    d_0 = minDepth(img, AtomsphericLight)
+    d_f = 8 * (DepthMap + d_0)
+    transmissionB, transmissionG, transmissionR = getRGBTransmissionESt(d_f)
 
-        blockSize = 9
-        gimfiltR = 50  # 引导滤波时半径的大小
-        eps = 10 ** -3  # 引导滤波时epsilon的值
+    transmission = refinedtransmissionMap(transmissionB, transmissionG, transmissionR, img)
+    sceneRadiance = sceneRadianceRGB(img, transmission, AtomsphericLight)
 
-        DepthMap = depthMap(img)
-        DepthMap = global_stretching(DepthMap)
-        guided_filter = GuidedFilter(img, gimfiltR, eps)
-        refineDR = guided_filter.filter(DepthMap)
-        refineDR = np.clip(refineDR, 0,1)
+    return sceneRadiance
 
-        cv2.imwrite('OutputImages/' + prefix + '_ULAPDepthMap.jpg', np.uint8(refineDR * 255))
+def run(args):
+    root_path = "/home/tongjiayan/UnderwaterImageRestoration/densedepth_based_restoration/datasets"
+    dataset_path = os.path.join(root_path, args.dataset)
+    score = open(args.score, mode = 'a',encoding='utf-8')
+    np.seterr(over='raise') # 触发FloatingPointError
+    time_start = time.time()
+    for image_name in os.listdir(dataset_path):
+        image_path = os.path.join(dataset_path, image_name)
+        if os.path.isfile(image_path):
+            print('********    file   ********',image_name)
+            img = cv2.imread(image_path) # BGR格式
+            img_RGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) # 转换成RGB格式
+            print("Get score...")
+            UICM, UISM, UIConM, UIQM, UCIQE = getScore(np.array(img_RGB))
+            print(image_name, file=score)
+            print("{0}\t{1}\t{2}\t{3}\t{4}".format(UICM, UISM, UIConM, UIQM, UCIQE), file=score)
+            print("Finished to get score!")
+            try:
+                sceneRadiance = getRecoverScene(img)
+            except FloatingPointError:
+                print("Fail to recover " + image_name + "!")
+            else:
+                sceneRadiance_RGB = cv2.cvtColor(sceneRadiance, cv2.COLOR_BGR2RGB)
+                UICM, UISM, UIConM, UIQM, UCIQE = getScore(np.array(sceneRadiance_RGB))
+                print("{0}\t{1}\t{2}\t{3}\t{4}".format(UICM, UISM, UIConM, UIQM, UCIQE), file=score)
+                save_path = os.path.join("OutputImages", args.dataset)
+                output_image = image_name.split('.')[0] + '_GBdehazing.jpg'
+                cv2.imwrite(os.path.join(save_path, output_image), sceneRadiance)
+                print("Done!")
+    
+    time_end = time.time()
+    print("TIME COST = {0}".format(time_end-time_start), file=score)
+    score.close()
 
-        AtomsphericLight = BLEstimation(img, DepthMap) * 255
-
-        d_0 = minDepth(img, AtomsphericLight)
-        d_f = 8 * (DepthMap + d_0)
-        transmissionB, transmissionG, transmissionR = getRGBTransmissionESt(d_f)
-
-        transmission = refinedtransmissionMap(transmissionB, transmissionG, transmissionR, img)
-        sceneRadiance = sceneRadianceRGB(img, transmission, AtomsphericLight)
-
-
-        cv2.imwrite('OutputImages/' + prefix + '_ULAP_TM.jpg', np.uint8(transmission[:, :, 2] * 255))
-
-
-        # print('AtomsphericLight',AtomsphericLight)
-
-        cv2.imwrite('OutputImages/' + prefix + '_ULAP.jpg', sceneRadiance)
-
-
-Endtime = datetime.datetime.now()
-Time = Endtime - starttime
-print('Time', Time)
+if __name__=='__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--score', default='score.log', help='score log')
+    parser.add_argument('--dataset', default='SQUID', help='Input image directory')
+    args = parser.parse_args()
+    run(args)
 
 
